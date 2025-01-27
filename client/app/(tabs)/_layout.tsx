0e7fa451036d2f81fa2a788a540734c886394d20
Juna <115675811+junakim0118@users.noreply.github.com>
2025-01-27 15:45:34 -0500

    registration form commit - basics done

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -28,6 +28,15 @@ export default function TabLayout() {
 			}}
 		>
 			<Tabs.Screen
+				name='register'
+				options={{
+					title: 'Register',
+					tabBarIcon: ({ color }) => (
+						<IconSymbol size={28} name='house.fill' color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
 				name='index'
 				options={{
 					title: 'Home',
