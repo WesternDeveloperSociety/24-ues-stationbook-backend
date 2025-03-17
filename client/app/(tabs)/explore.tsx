cd62edcb5a8b929877d9bdddfc1efdc4575321e7
Zanan Virani <viranizanan@gmail.com>
2025-03-17 16:27:21 -0400

    Added Qr Code

--- client/app/(tabs)/explore.tsx
+++ client/app/(tabs)/explore.tsx
@@ -1,11 +1,25 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, useWindowDimensions } from 'react-native';
 import { Colors } from '../../constants/Colors';
+import QRCode from 'react-native-qrcode-svg';
+import { useState } from 'react';
 
 export default function TabTwoScreen() {
+	const dimensions = useWindowDimensions();
+
+	const [userId, setUserId] = useState<string | undefined>(undefined);
+
 	return (
 		<View style={{ flex: 1, backgroundColor: Colors.white }}>
-			<View style={{ height: 100, alignItems: 'center', justifyContent: 'center' }}>
+			<View style={{ height: 150, alignItems: 'center', justifyContent: 'center' }}>
 				<Text style={styles.title}>My QR Code</Text>
+			</View>
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<QRCode
+					value={userId ?? 'hello'}
+					size={dimensions.width * 0.7}
+					color={Colors.light.text}
+					backgroundColor={Colors.white}
+				/>
 			</View>
 		</View>
 	);
@@ -13,7 +27,7 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
 	title: {
-		fontSize: 32,
+		fontSize: 50,
 		fontWeight: 'bold',
 		textAlign: 'center',
 	},
