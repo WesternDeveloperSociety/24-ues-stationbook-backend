44f0ee146845fc469de75146217603421870ce43
Anna Liu <aliu426@uwo.ca>
2025-03-24 14:25:28 -0400

    updated schema

--- DATABASE/schema.sql
+++ DATABASE/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE ues_stationbook;
 
 USE ues_stationbook;
 
-CREATE TABLE students (
+CREATE TABLE student (
 	student_id 			INT(10) UNIQUE NOT NULL PRIMARY KEY,
     email 				VARCHAR(20) UNIQUE NOT NULL,
     first_name			VARCHAR(20) NOT NULL,
@@ -12,6 +12,33 @@ CREATE TABLE students (
     points				INT(30),
     is_admin			BOOLEAN DEFAULT false,
     created_at			TIMESTAMP DEFAULT current_timestamp,
-    updated_at			TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp
+    last_updated		TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp
 	);
     
+CREATE TABLE event (
+	event_no		INT(10) UNIQUE NOT NULL PRIMARY KEY,
+    name			VARCHAR(20) NOT NULL,
+    date			DATE,
+    description		VARCHAR(200),
+    image			BLOB,
+    points			INT(5),
+    track			VARCHAR(40)
+);
+
+CREATE TABLE scan (
+	student_id  INT(10) NOT NULL,
+    event_no    INT(10) NOT NULL,
+    scanned_at  TIMESTAMP DEFAULT current_timestamp,
+    PRIMARY KEY (student_id, event_no),
+    FOREIGN KEY (student_id) REFERENCES student(student_id) ON DELETE CASCADE,
+    FOREIGN KEY (event_no) REFERENCES event(event_no) ON DELETE CASCADE
+);
+
+
+
+
+
+
+
+
+
